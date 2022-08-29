d277540eeb92637a92580bce6400a55a99342ccd
ssingh54 <ssingh54@ncsu.edu>
2022-08-28 22:43:55 -0400

    added mypy in precommit

--- code/tests/unit/test_calculator.py
+++ code/tests/unit/test_calculator.py
@@ -1,8 +1,8 @@
-from unittest.mock import patch
 from code.main import add
+from unittest.mock import Mock, patch
 
 
 @patch("code.main.add")
-def test_add(adder):
+def test_add(adder: Mock):
     adder.return_value = 5
     assert adder.return_value == add(2, 3)
